f54ca1a70e47380e083147806f704c6441e552e7
Grégoire Neuville <gregoire.neuville@gmail.com>
2015-07-09 01:01:57 +0200

    Finished Exo1.1

--- src/Exos1.hs
+++ src/Exos1.hs
@@ -1,16 +1,52 @@
+{-# OPTIONS_GHC -Wall #-}
+
 module Exos1  where
 
-toDigits :: Integer -> [Integer]
-toDigits d | d <= 0 = []
-toDigits d = map charToInt $ show d
+validate :: Integer -> Bool
+validate d = remainder d == 0
   where
-    charToInt = read . (:[])
+    remainder =
+      snd . divByTen . (sumDigits . doubleEveryOther . toDigits)
+    divByTen = (flip quotRem) 10
+
+toDigits :: Integer -> [Integer]
+toDigits = myReverse . toDigitsRev
 
 toDigitsRev :: Integer -> [Integer]
-toDigitsRev = reverse . toDigits
+toDigitsRev d = decompose $ quotRem d 10
+  where
+    decompose (q, r) | q == 0 = [r]
+                     | otherwise = r : toDigitsRev q
+
+-- toDigits d
+--   | d <= 0 = []
+--   | otherwise = [charToInt x | x <- show d]
+--   where
+--     charToInt = read . (:[])
+
+-- toDigitsRev :: Integer -> [Integer]
+-- toDigitsRev = myReverse . toDigits
 
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther [] = []
-doubleEveryOther [x] = [x * 2]
-doubleEveryOther [x, y] = [x * 2, y]
-doubleEveryOther (x:xs) = x : doubleEveryOther xs
+doubleEveryOther = deo . myReverse
+  where
+    deo [] = []
+    deo [x] = [x]
+    deo [x, y] = [y * 2, x]
+    deo (x:y:xs) = deo xs ++ [y * 2, x]
+
+sumDigits :: [Integer] -> Integer
+sumDigits ds = sumL $ concatL [toDigits d | d <- ds] []
+  where
+    concatL [] acc = acc
+    concatL ([]:l) acc = concatL l acc
+    concatL ((x:xs):ls) acc = concatL (xs:ls) (x:acc)
+    sumL [] = 0
+    sumL (x:xs) = x + sumL xs
+
+myReverse :: [t] -> [t]
+myReverse [] = []
+myReverse l = rev l []
+  where
+    rev [] ys = ys
+    rev (x:xs) ys = rev xs $ x:ys
